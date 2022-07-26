7d149399948e5fe433c7bf3b892fe325e58d175d
ngwesinsandar2 <ngwesinsandar112@gmail.com>
2022-07-26 19:50:14 +0630

    themeBtnandButtonsUpdate

--- src/components/Theme/ThemeBtn.jsx
+++ src/components/Theme/ThemeBtn.jsx
@@ -1,10 +1,10 @@
 import { Stack } from '@mui/material';
 import React from 'react';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
 import ThemeStyle from "./themeBtn.module.css"
 import { ThemeContext } from '../../App';
 import { useContext } from 'react';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
 
 const ThemeBtn = () => {
     const value = useContext(ThemeContext)
@@ -13,7 +13,7 @@ const ThemeBtn = () => {
         <>
             <Stack onClick={value.switchTheme} className={ThemeStyle.btnCon}>
                 {
-                    value.theme === "light" ? <DarkModeIcon /> : <LightModeIcon />
+                    value.theme === "light" ? <AcUnitIcon /> : <LocalFireDepartmentIcon />
                 }
             </Stack>
         </>
