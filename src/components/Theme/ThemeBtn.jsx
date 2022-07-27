9db86bbe690ca10251451fd8a618f1c9f83dce22
ngwesinsandar2 <ngwesinsandar112@gmail.com>
2022-07-27 10:33:49 +0630

    themeBtnUpdate

--- src/components/Theme/ThemeBtn.jsx
+++ src/components/Theme/ThemeBtn.jsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mui/material';
+import { Box } from '@mui/material';
 import React from 'react';
 import ThemeStyle from "./themeBtn.module.css"
 import { ThemeContext } from '../../App';
@@ -11,11 +11,11 @@ const ThemeBtn = () => {
 
     return ( 
         <>
-            <Stack onClick={value.switchTheme} className={ThemeStyle.btnCon}>
+            <Box onClick={value.switchTheme} className={ThemeStyle.btnCon}>
                 {
                     value.theme === "light" ? <AcUnitIcon /> : <LocalFireDepartmentIcon />
                 }
-            </Stack>
+            </Box>
         </>
      );
 }
